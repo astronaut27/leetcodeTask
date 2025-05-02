92f627f4095692faff8dd946e405b0a57467001f
astronaut27 <desperate.astronaut@gmail.com>
2025-05-02 14:45:32 +0200

    remove adjacent duplicates

--- array/remove_adjacent_duplicates/main.go
+++ array/remove_adjacent_duplicates/main.go
@@ -13,7 +13,7 @@ func main() {
 func removeDuplicates(s string) string {
 	b := []byte(s)
 	for i := len(b) - 1; i > 0; i-- {
-		if i != len(b) && b[i] == b[i-1] {
+		if b[i] == b[i-1] {
 			b = append(b[:i-1], b[i+1:]...)
 		}
 	}
